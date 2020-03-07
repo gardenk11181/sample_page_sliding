3c786dc20d97bbca1f4f4c7b5334e058c9b57653
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-03-07 20:40:22 +0900

    Page Sliding

--- app/src/main/java/com/gardenlab/samplepagesliding/MainActivity.java
+++ app/src/main/java/com/gardenlab/samplepagesliding/MainActivity.java
@@ -3,12 +3,71 @@ package com.gardenlab.samplepagesliding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isPageOpen = false;
+
+    Animation translateLeftAnim;
+    Animation translageRightAnim;
+
+    LinearLayout page;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        page = findViewById(R.id.page);
+
+        translateLeftAnim = AnimationUtils.loadAnimation(this,R.anim.translate_left);
+        translageRightAnim = AnimationUtils.loadAnimation(this,R.anim.translate_left);
+
+        SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
+        translateLeftAnim.setAnimationListener(animListener);
+        translageRightAnim.setAnimationListener(animListener);
+
+        button  = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPageOpen) {
+                    page.startAnimation(translageRightAnim);
+                } else {
+                    page.setVisibility(View.VISIBLE);
+                    page.startAnimation(translateLeftAnim);
+                }
+            }
+        });
+
+    }
+
+    private class SlidingPageAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            if(isPageOpen) {
+                page.setVisibility(View.INVISIBLE);
+
+                button.setText("Open");
+                isPageOpen = false;
+            } else {
+                button.setText("true");
+                isPageOpen=true;
+            }
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
